248dd478b27d497244a54c16617d6967e18a3f96
ajay25292019 <ajay25292019@outlook.com>
2021-05-13 12:42:48 +0530

    teacher and custom request

---
+++ jay.school.contracts/Entities/Classes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jay.school.contracts.Entities
+{
+    public class Classes
+    {
+        public string Standerd { get; set; }
+        public List<string> Section { get; set; }
+        public List<string> Subjects { get; set; }
+    }
+}
---
+++ jay.school/Controllers/TeachersController.cs
@@ -0,0 +1,40 @@
+﻿using jay.school.contracts.Contracts;
+using jay.school.contracts.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jay.school.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersController : ControllerBase
+    {
+        private readonly ITeachersService _teachersService;
+        public TeachersController(ITeachersService teachersService)
+        {
+            _teachersService = teachersService;
+        }
+        [Route("teachers")]
+        [HttpGet]
+        public async Task<ActionResult<CustomResponse<List<Teacher>>>> GetTeachers()
+        {
+            return await _teachersService.GetTeachers();
+        }
+        [Route("teacher/{id}")]
+        [HttpGet]
+        public async Task<ActionResult<CustomResponse<Teacher>>> GetTeacher(int id)
+        {
+            return await _teachersService.GetTeacher(id);
+        }
+        [Route("add")]
+        [HttpPost]
+        public async Task<ActionResult<CustomResponse<string>>> AddTeachers(CustomRequest<List<Teacher>> request)
+        {
+            return await _teachersService.AddTeachers(request);
+        }
+    }
+}
