4f87b7a5173385f41048299cfa749e25759850d2
Sujay hosmani <hosmani.sujay@gmail.com>
2021-07-09 12:10:43 +0530

    dscdsc

--- jay.school.contracts/Entities/AttendanceResult.cs
+++ jay.school.contracts/Entities/AttendanceResult.cs
@@ -1,17 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+
 
 namespace jay.school.contracts.Entities
 {
-    public class AttendanceResult
-    {
-        public List<OverAll> TotalAvg { get; set; }
-    
-    }
-
     public class OverAll
     {
         public string Subject { get; set; }
